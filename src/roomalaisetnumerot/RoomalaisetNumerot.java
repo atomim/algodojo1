3025de22969d724bf128e7b345e6553d447770f4
olavilin <olavilin@dhp-77.cs.helsinki.fi>
2013-04-18 19:19:27 +0300

    Poistettiin ylimääräistä koodia (Ja Herobrine)

--- src/roomalaisetnumerot/RoomalaisetNumerot.java
+++ src/roomalaisetnumerot/RoomalaisetNumerot.java
@@ -17,52 +17,7 @@ public class RoomalaisetNumerot {
             return lol + rekursoi(luku % 10);
         }
         String vastaukset[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
-        if (luku == luku) {
-            return vastaukset[luku];
-        }
-
-        if (luku == 4) {
-            return "IV";
-        }
-        if (luku == 5) {
-            return "V";
-        }
-        if (luku == 9) {
-            return "IX";
-        }
-        if (luku == 10) {
-            return "X";
-        }
-        if (luku == 50) {
-            return "L";
-        }
-        if (luku == 100) {
-            return "C";
-        }
-        if (luku == 500) {
-            return "D";
-        }
-
-        if (luku > 500) {
-            return ("D" + rekursoi(luku - 500)).replace("DCD", "CM");
-        }
-        if (luku > 100) {
-            return ("C" + rekursoi(luku - 100)).replace("CCCC", "CD");
-        }
-        if (luku > 50) {
-            return ("L" + rekursoi(luku - 50)).replace("LXL", "XC");
-        }
-        if (luku > 10) {
-            return ("X" + rekursoi(luku - 10)).replace("XXXX", "XL");
-        }
-        if (luku > 5) {
-            return "V" + rekursoi(luku - 5);
-        }
-        String pelle = "";
-        for (int i = 0; i < luku; i++) {
-            pelle = pelle + "I";
-        }
-        return pelle;
+        return vastaukset[luku];
     }
 
     public String tekstiksi(int luku) {
